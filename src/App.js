ddff6434878b09bfa57f318454023a406d98d623
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-15 15:28:57 +0300

    todo app updated with state and class

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import TodoItem from './components/TodoItem'
 import todosData from './todosData'
 
 
-function App() {
-  const todoComponent = todosData.map(todo =>  {
-    return(
-      <TodoItem key = {todo.id} todo = {todo}/>
-    )
-  })
-  return (
-    <div className = "todo-list">
-    
-    {todoComponent}
+class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+
   
-    </div>
-  );
+  render(){
+
+    const todoComponent = this.state.todos.map(todo =>  {
+      return(
+        <TodoItem key = {todo.id} todo = {todo}/>
+      )
+    })
+    return (
+      <div className = "todo-list">
+      
+      {todoComponent}
+    
+      </div>
+    );
+  }
+  
 }
 
 export default App;
