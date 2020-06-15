a5aa05da6cffc47084c4284b46caf35eefbb2ce8
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-15 15:56:40 +0300

    Event handlers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-
+function handleClick() {
+  console.log("I was clicked")
+}
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+     <img onMouseOver={() => console.log("Hovered")} src="https://www.fillmurray.com/200/100" alt=""/>
+     <br/>
+     <br/>
+     <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
