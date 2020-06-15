932cffcec35ecb2c68fdba7b5d7eb4384a056334
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-15 14:04:19 +0300

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import TodoItem from './components/TodoItem'
+import todosData from './todosData'
 
 
 function App() {
+  const todoComponent = todosData.map(todo =>  {
+    return(
+      <TodoItem key = {todo.id} todo = {todo}/>
+    )
+  })
   return (
     <div className = "todo-list">
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-      
+    
+    {todoComponent}
   
     </div>
   );
