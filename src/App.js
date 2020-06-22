41bbf7cd9d915218d4428ad9cf9b3ffc1a6829ca
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-22 15:32:35 +0300

    .git setState2

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ class App extends React.Component{
     this.state = {
       count: 0
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+      // this.setState(newVersionOfState)
+      this.setState(prevState =>{
+      return {
+        count: prevState.count + 1
+      }
+        })
   }
 
   render(){
     return(
       <div>
         <h1>{this.state.count}</h1>
-        <button>Change!</button>  
+        <button onClick = {this.handleClick}>Change!</button>  
       </div>
     )
   }
