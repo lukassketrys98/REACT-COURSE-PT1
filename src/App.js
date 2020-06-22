dc73334d494db99612643e6e245ef0f60dcec998
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-22 15:15:01 +0300

    setstate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+  render(){
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button>Change!</button>  
+      </div>
+    )
+  }
 }
 
 export default App;
