115fb0b840ce770d74aafcd57a33c96c6259ff95
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-23 14:42:06 +0300

    todo app update

--- src/App.js
+++ src/App.js
@@ -10,19 +10,50 @@ class App extends React.Component{
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+    
   }
 
+  handleChange(id){
+   
+      this.setState(prevState =>{
+    
+        const updatedTodos = prevState.todos.map(todo =>{
+          // console.log(todo)
+          if(todo.id === id){
+        
+
+           return {
+             ...todo,
+             completed: !todo.completed
+           }
+
+            // console.log(todo)
+          }
+          return todo
+        })
+        // console.log(updatedTodos)
+        console.log(prevState.todos)
+        console.log(updatedTodos)
+          return {
+            todos: updatedTodos
+          }
+    
+      })
+
+  
+  }
   
   render(){
 
     const todoComponent = this.state.todos.map(todo =>  {
       return(
-        <TodoItem key = {todo.id} todo = {todo}/>
+        <TodoItem key = {todo.id} todo = {todo} handleChange = {this.handleChange}/>
       )
     })
     return (
       <div className = "todo-list">
-      
+  
       {todoComponent}
     
       </div>
