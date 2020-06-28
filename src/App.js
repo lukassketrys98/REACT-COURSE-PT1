097ff9a48a295e9a51cf5f2d5e77df2b795255d3
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-28 17:36:07 +0300

    done project

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      firstName: '',
-      lastName: '',
-      isFriendly: true,
+      name:'',
+      surname: '',
+      age: 0,
       gender: '',
-      favColor: 'blue'
+      destination: '',
+      Sugar: false,
+      Carbs: false,
+      Protein: false,
+      isRestricted: true
     }
     this.handleChange = this.handleChange.bind(this)
   }
-  handleChange(event) {
-    const {name, value, type,checked} = event.target
-    type === "checkbox"? this.setState({[name]: checked}): this.setState(
-      { 
-        [name]: value
-      }
-      )
+  handleChange(event){
+    const {name,value, type, checked} = event.target
+   type==="checkbox"? 
+   this.setState({
+        [name]: checked
+    }) : 
+   this.setState({[name]: value})
   }
   render() {
   
@@ -26,63 +30,46 @@ class App extends Component {
     return (
       <div>
         <form>
-          {/* text */}
-          <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name" />
-          <br />
-          <input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name" />
-          <h1>{this.state.firstName} {this.state.lastName}</h1>
-          {/* textarea */}
-          <textarea 
-                    value={"Some default value"}
-                    onChange={this.handleChange}
-                />
-          {/* input type checkbox */}
-          <br />
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="isFriendly"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Is friendly?
-                </label>
-          {/* input type radio */}
-
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="male"
-                        checked={this.state.gender === "male"}
-                        onChange={this.handleChange}
-                    /> Male
-                </label>
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="female"
-                        checked={this.state.gender === "female"}
-                        onChange={this.handleChange}
-                    /> Female
-                </label>
-                <label>You are a {this.state.gender}</label>
-          {/* input type select*/}
-          <h1>Favorite color: </h1>
-          <select 
-          value = {this.state.favColor}
-          onChange = {this.handleChange}
-          name='favColor'
-          >
-            <option value="blue">Blue</option>
-            <option value="green">Green</option>
-            <option value="orange">Orange</option>
+          <label>Name: </label>
+          <input name = "name" onChange = {this.handleChange} value={this.state.name} type="text" placeholder = "Enter your name.."/>
+          <br/>
+          <label>Surname: </label>
+          <input name = "surname" value = {this.state.surname} onChange = {this.handleChange} type="text" placeholder = "Enter your name.."/>
+          <br/>
+           <label>Age: </label>
+          <input value = {this.state.age} name = "age" onChange = {this.handleChange} type="text" placeholder = "Enter your name.."/>
+          <br/>
+          <label>Gender: </label>
+          <label>Male</label>
+          <input name = "gender" checked = {this.state.gender === "male"} onChange={this.handleChange} value = "male" type="radio" />
+          <label>Female</label>
+          <input name = "gender" checked = {this.state.gender === "female"} onChange={this.handleChange} value = "female" type="radio"/>
+          <br/>
+          <label>Destination: </label>
+          <select name = "destination" onChange={this.handleChange} value = {this.state.destination}>
+            <option value="Spain">Spain</option>
+            <option value="Colombia">Colombia</option>
+            <option value="Venesuela">Venesuela</option>
           </select>
-      <h1>Your favorite color is {this.state.favColor}</h1>
-      <button>Submit</button>
+          <br/>
+          <label>Dietary restrictions: </label>
+          <input onChange={this.handleChange} checked = {this.state.Sugar} name = "Sugar" type="checkbox"/> Sugar
+          <input onChange={this.handleChange} checked = {this.state.Protein} name = "Protein" type="checkbox"/> Protein
+          <input value = {this.state.restrictions} onChange={this.handleChange} checked = {this.state.Carbs} name = "Carbs" type="checkbox"/> Carbs
+          <br/>
+          <button>Submit</button>
         </form>
+        <hr/>
+        <h2>Entered information: </h2>
+        <p>Your age: {this.state.age} </p>
+        <p>Your name: {this.state.name} </p>
+        <p>Your surname: {this.state.surname} </p>
+        <p>Your gender: {this.state.gender}</p>
+        <p>Your destination: {this.state.destination} </p>
+        <p>Your dietary restrictions: 
+          {this.state.Sugar ? "For Sugar, " : ""} 
+          {this.state.Protein  ? "For Protein, " : ""}
+          {this.state.Carbs  ? "For Carbs " : ""}</p>
       </div>
     )
   }
