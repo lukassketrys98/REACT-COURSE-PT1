254957d1157db5f9669c161ab6365c6eef1e22d8
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-06-28 18:13:09 +0300

    UPDATE

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class MemeGenerator extends Component{
+    constructor(){
+        super()
+        this.state = {
+            topText: '',
+            bottomText: '',
+            randomImage: 'http://i.imgflip.com/1bij.jpg',
+            allMemeImgs: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://api.imgflip.com/get_memes').then(response => response.json())
+        .then(data => {
+            this.setState({
+                allMemeImgs: data.memes
+            })
+        })
+    }
+
+    render(){
+        return (
+            <div>
+              <h1>Hello world</h1>
+            </div>
+          )
+    }
+ 
+}
+
+export default MemeGenerator;
